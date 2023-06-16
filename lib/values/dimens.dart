25501d20a0eb03505c943bc167f2bb992ab269d0
lordyhas <dev.haspro@gmail.com>
2023-06-16 09:46:39 +0200

    version 1.0.0+3 | component added

---
+++ lib/values/dimens.dart
@@ -0,0 +1,20 @@
+part of values;
+
+
+
+
+const double kMinDimens = 520.0;
+const double kTabDimens = 720.0;
+const double kDeskDimens = 1028.0;
+const kPhoneDimens = 620.0;
+const kPhoneXDimens = 720.0;
+const double kMediumDimens = 720.0;
+const double kMedium2Dimens = 1028.0;
+
+
+abstract class ConstraintsDimens{}
+
+
+abstract class ScreenDimens{}
+
+
---
+++ lib/values/values.dart
@@ -0,0 +1,9 @@
+library values;
+
+import 'dart:ui';
+
+
+part 'dimens.dart';
+part 'style.dart';
+part 'string.dart';
+part 'colors.dart';
