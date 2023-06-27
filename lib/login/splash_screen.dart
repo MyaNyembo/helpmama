6f9ed86c32a681cf45e136fa4b6b1e17cd62e2b8
lordyhas <dev.haspro@gmail.com>
2023-06-27 15:11:59 +0200

    login bloc added

--- lib/login/splash_screen.dart
+++ lib/login/splash_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
-import 'navigation.dart';
+import '../navigation.dart';
 
 
 class MyCustomWidget extends StatefulWidget {
